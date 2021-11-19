7a08c9ba507967ad2bdfba616e2b2ac7bd9038b8
Cristian Velasquez <cristianvelasquezp@gmail.com>
2021-11-18 20:38:49 -0600

    add checkout and account section

--- page.php
+++ page.php
@@ -4,7 +4,11 @@ get_header();
 <main class="main">
     <div class="container">
         <div class="row">
+            <?php if( is_cart() || is_checkout() || is_account_page() ) { ?>
+            <div>
+            <?php } else { ?>
             <div class="col-1-of-2">
+            <?php } ?>
                 <h1 class="heading-tertiary heading-main"><?php the_title() ?></h1>
                 <?php the_content() ?>
             </div>
